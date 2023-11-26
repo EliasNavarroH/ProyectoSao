2464e1a81ba02af7948d4422ba9dac021e2dbaa8
Elias Navarro <eliasnavarro@Eliass-MacBook-Air.local>
2023-11-25 22:41:38 -0300

    Se agrego el codigo para ingresar asignaturas

--- src/App.java
+++ src/App.java
@@ -5,17 +5,27 @@ public class App {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Bienvenidos al Sistema de Gestion de Califiaciones");
         //Ingresar logo con compañeros 
+        IngresoAsginaturas();
     }
     //se inicializa la clase para ingresar la cantidad de asignaturas
     public static void IngresoAsginaturas(){
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Cuantas asignaturas quieres ingresar ?");
+        System.out.println("Cuantas asignaturas quieres ingresar ? \nNo puedes Ingresar mas de 10");
         int cantidadAsignaturas = entrada.nextInt();
+        entrada.nextLine();// se agrega este nextline porque luego del nextint el scanner queda una string vacia y hace que se utilize en el bucle de abajo
+        String asignaturas[];
+        asignaturas = new String[cantidadAsignaturas];
+        int j = 1;
+        for(int i = 0 ; i < asignaturas.length ; i++ ){
+            System.out.println("Ingrese la Asignatura nro: " + j);
+            asignaturas[i] = entrada.nextLine();
+            j++;
+            }
     }
     public static void IngresoEstudiantes(){
 
     }
     public static void IngresoCalificacione(){
-        
+
     }
 }
