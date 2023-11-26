71d9e4ae3b85ff7c5a9d5f87a2462fb6906fa059
Elias Navarro <eliasnavarro@Eliass-MacBook-Air.local>
2023-11-25 22:20:40 -0300

    Se crea la clase y estructura del programa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Bienvenidos al Sistema de Gestion de Califiaciones");
+        //Ingresar logo con compañeros 
+    }
+    //se inicializa la clase para ingresar la cantidad de asignaturas
+    public static void IngresoAsginaturas(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Cuantas asignaturas quieres ingresar ?");
+        int cantidadAsignaturas = entrada.nextInt();
+    }
+    public static void IngresoEstudiantes(){
+
+    }
+    public static void IngresoCalificacione(){
+        
     }
 }
