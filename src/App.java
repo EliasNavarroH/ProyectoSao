954069a3d3ebffd2650b3b40a6ec91ef1d39719f
Zanty1995 <santiago.mossi@gmail.com>
2023-12-04 12:18:29 +0100

    Corrección de clase. línea 4

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Random;
-public class AmbientePruebas2 {
+public class App {
     
 
     public static void main(String[] args) throws Exception {
